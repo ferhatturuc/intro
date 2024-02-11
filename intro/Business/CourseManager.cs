ca2cc9d7c56c2159e9197be6ebc67aed8594f3b4
Ferhat Turuç <ferhat.turuc@outlook.com>
2024-02-11 20:42:45 +0300

    Add project files.

---
+++ intro/Business/CourseManager.cs
@@ -0,0 +1,47 @@
+﻿using intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[3];
+    //constructor
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C#";
+        course1.Description = ".Net 8 vs...";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Id = 1;
+        course2.Name = "JAVA";
+        course2.Description = "Java 17 vs...";
+        course2.Price = 10;
+
+        Course course3 = new Course();
+        course3.Id = 1;
+        course3.Name = "Python";
+        course3.Description = "Python 3.12 vs...";
+        course3.Price = 20;
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+
+        //Course[] courses = { course1, course2, course3};
+    }
+    public Course[] GetAll()
+    {
+        //veri kaynağından çekilir...
+        Console.WriteLine("Kurslar listelendi");
+
+        return courses;
+    }
+}
---
+++ intro/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using intro.Business;
+using intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+//variables --> camelCase
+bool isAuthenticated = true;
+
+Console.WriteLine(message1);
+
+//condition
+if (isAuthenticated == true )
+{
+    Console.WriteLine("Hoşgeldin");
+}
+else
+{
+    Console.WriteLine("Giriş yap");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3 ", "Kredi 4", "Kredi 5", "Kredi 6" };
+
+string[] loans2 = new string[6];
+loans2[0] = "Kredi 1";
+
+//   start     condition         increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new();
+Course[] courses = courseManager.GetAll();
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+}
+
+Console.WriteLine("kod bitti şimdilik...");
